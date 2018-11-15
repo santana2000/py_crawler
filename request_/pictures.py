8deb03cf2a14056d47476a908dac71e7c7667f45
tesla <532014809@qq.com>
2018-11-15 11:51:08 +0800

    pic top250

--- request_/pictures.py
+++ request_/pictures.py
@@ -1,16 +1,27 @@
 import urllib
 import re
+import time
+
+from urllib import  request
 import requests
 from bs4 import BeautifulSoup
 
+# https://book.douban.com/top250?start=0
+# https://book.douban.com/top250?start=25
 
+x = 1
+for i in range(2):  #0~9
+    url = 'https://book.douban.com/top250?start={}'.format(i * 25)
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,'html.parser')
 
-res = requests.get('https://book.douban.com/top250')
+    # imgs = soup.select('img')
+    imgs = soup.find_all('img',{'width':'90'})
 
-soup = BeautifulSoup(res.text,'html.parser')
+    for img in imgs:
+        print(img['src'])
+        request.urlretrieve(img['src'], 'C:/Users/tesla/Desktop/top250\%s.jpg'%x)
+        x += 1
 
-print(soup.select('img'))
+    time.sleep(1)
 
-f = open(file_name, 'wb')
-f.write(res.content)
-f.close()
