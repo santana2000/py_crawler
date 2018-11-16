e29fdd2bad040c1f9ca1967a4a98e30211c7178c
tesla <532014809@qq.com>
2018-11-16 15:25:18 +0800

    numpy库与matplotlib库

--- request_/book250.py
+++ request_/book250.py
@@ -7,20 +7,20 @@ from lxml import etree
 import time   #这里导入时间模块，以免豆瓣封你IP
 import io
 
-with open('C:/Users/tesla/Desktop/book','wb',encoding='utf-8') as file:
 
-    url = 'https://book.douban.com/top250'
-    data = requests.get(url).text
-    topbooks = etree.HTML(data)
-    books = topbooks.xpath('//*[@id="content"]/div/div[1]/div/table')
 
-    for book in books:
-        name = book.xpath('./tr/td[2]/div[1]/a/@title')  # @title
-        score = book.xpath('./tr/td[2]/div[2]/span[2]/text()')
-        print('{} 的评分是: {}'.format(name,score))
-        # file.write('{} 的评分是: {}\n'.format(name,score))   #写入文件
+url = 'https://book.douban.com/top250'
+data = requests.get(url).text
+topbooks = etree.HTML(data)
+books = topbooks.xpath('//*[@id="content"]/div/div[1]/div/table')
 
-    time.sleep(1) #加个睡眠，防止IP被封
+for book in books:
+    name = book.xpath('./tr/td[2]/div[1]/a/@title')  # @title
+    score = book.xpath('./tr/td[2]/div[2]/span[2]/text()')
+    print('{} 的评分是: {}'.format(name,score))
+    # file.write('{} 的评分是: {}\n'.format(name,score))   #写入文件
+
+time.sleep(1) #加个睡眠，防止IP被封
 
 # //*[@id="content"]/div/div[1]/div/table[1]/tr/td[1]/a/img
 # //*[@id="content"]/div/div[1]/div/table[9]/tr/td[2]
--- request_/pictures_name.py
+++ request_/pictures_name.py
@@ -4,40 +4,40 @@
 
 import requests
 from lxml import etree
-import time   #这里导入时间模块，以免豆瓣封你IP
+import time   #导入时间模块，以免封IP
+
+for i in range(2):
+    url = 'https://book.douban.com/top250?start={}'.format(i*25)
+    data = requests.get(url).text
+    topbooks = etree.HTML(data)
+    books = topbooks.xpath('//*[@id="content"]/div/div[1]/div/table')
 
 
-url = 'https://book.douban.com/top250'
-data = requests.get(url).text
-topbooks = etree.HTML(data)
-books = topbooks.xpath('//*[@id="content"]/div/div[1]/div/table')
+    for book in books:
+        name = book.xpath('./tr/td[2]/div[1]/a/@title')[0] # @title
+        # score = book.xpath('./tr/td[2]/div[2]/span[2]/text()')
+        img_url = book.xpath('./tr/td[1]/a/img/@src')[0]
+        print('{} 的评分是: {}'.format(name,img_url))
 
+        img_content = requests.get(img_url).content
+        file_name = name + '.jpg'
+        with open('C:/Users/tesla/Desktop/book'+'/'+file_name, 'wb') as file:
+            file.write(img_content)
 
-for book in books:
-    name = book.xpath('./tr/td[2]/div[1]/a/@title') [0] # @title
-    # score = book.xpath('./tr/td[2]/div[2]/span[2]/text()')
-    img_url = book.xpath('./tr/td[1]/a/img/@src')[0]
-    print('{} 的评分是: {}'.format(name, img_url))
-
-    img_content = requests.get(img_url).content
-    file_name = name + '.jpg'
-    with open('C:/Users/tesla/Desktop/book'+'/'+file_name, 'wb') as file:
-        file.write(img_content)
-
-    time.sleep(1)  # 加个睡眠，防止IP被封
-
-
-
-
-    # file.write('{} 的评分是: {}\n'.format(name,score))   #写入文件
+        time.sleep(1)  # 加睡眠，防止IP被封
 
 
 
-# //*[@id="content"]/div/div[1]/div/table[1]/tr/td[1]/a/img
-# //*[@id="content"]/div/div[1]/div/table[9]/tr/td[2]
 
-# //*[@id="content"]/div/div[1]/div/table[1]//tr/td[1]/a/img 图片
+        # file.write('{} 的评分是: {}\n'.format(name,score))   #写入文件
 
-# //*[@id="content"]/div/div[1]/div/table[22]/tr/td[2]/div[1]/a/@title
-# //*[@id="content"]/div/div[1]/div/table[15]/tr/td[2]/div[1]/a
+
+
+    # //*[@id="content"]/div/div[1]/div/table[1]/tr/td[1]/a/img
+    # //*[@id="content"]/div/div[1]/div/table[9]/tr/td[2]
+
+    # //*[@id="content"]/div/div[1]/div/table[1]//tr/td[1]/a/img 图片
+
+    # //*[@id="content"]/div/div[1]/div/table[22]/tr/td[2]/div[1]/a/@title
+    # //*[@id="content"]/div/div[1]/div/table[15]/tr/td[2]/div[1]/a
 
